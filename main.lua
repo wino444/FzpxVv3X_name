bfe54937d26f1c63b8f13f99d92d0ac367a931fa
wino444 <146556853+wino444@users.noreply.github.com>
2025-05-02 06:48:21 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,24 @@
 return {
     players = {
         "euiivdyj",
+        "jgjkjgj4",
         "leo40327",
-        "jgjkjgj4"
+        "PlayerName004",
+        "PlayerName005",
+        "PlayerName006",
+        "PlayerName007",
+        "PlayerName008",
+        "PlayerName009",
+        "PlayerName010",
+        "PlayerName011",
+        "PlayerName012",
+        "PlayerName013",
+        "PlayerName014",
+        "PlayerName015",
+        "PlayerName016",
+        "PlayerName017",
+        "PlayerName018",
+        "PlayerName019",
+        "PlayerName020"
     }
 }
